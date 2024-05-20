2cb8d0e6653b931bd95ed59eab09e11eb55c341c
crxxnk <trickythebrawler@gmail.com>
2024-05-20 21:05:24 +0300

    yo

---
+++ script.js
@@ -0,0 +1,21 @@
+const songList =
+[
+  {
+    name: "MADONNA & RIHANNA",
+    artist: "Rich Amiri",
+    src: "1.mp3",
+    cover: "1.jpg"
+  },
+  {
+    name: "Type Shit",
+    artist: "Future, Metro Boomin, Travis Scott, Playboi Carti",
+    src: "2.mp3",
+    cover: "2.png"
+  },
+  {
+    name: "NOSTYLIST",
+    artist: "Destroy Lonely",
+    src: "3.mp3",
+    cover: "3.webp"
+  }
+]
